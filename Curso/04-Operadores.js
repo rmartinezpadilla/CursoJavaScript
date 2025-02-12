83be6832461e454062ab21c9c9dcb2b9159664d8
ruben <ruben.martinez@t-evolvers.com>
2025-02-12 06:11:45 -0500

    ejercicios y operadores

---
+++ Curso/04-Operadores.js
@@ -0,0 +1,21 @@
+// Operadores
+
+// Operadores Aritméticos
+
+let a = 5
+let b = 10
+
+console.log(5 + 10) // suma
+console.log(a + b) // suma
+console.log(a - b) // resta
+console.log(a * b) // multiplicación
+console.log(a / b) // Divisón
+
+console.log(a % b) // Módulo
+console.log(a ** b) // Exponente
+
+a++ // Incremento
+console.log(a) // Incremento
+
+b-- // Decremento
+console.log(b) // Decremento

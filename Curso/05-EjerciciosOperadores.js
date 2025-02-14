69820d1e730c36376359decae97864315c88eebe
ruben <ruben.martinez@t-evolvers.com>
2025-02-14 16:12:29 -0500

    realizando ejercicios de operaciones

--- Curso/05-EjerciciosOperadores.js
+++ Curso/05-EjerciciosOperadores.js
@@ -1,14 +1,20 @@
-/*
-Clase 20 - Ejercicios: Operadores
-Vídeo: https://youtu.be/1glVfFxj8a4?t=6458
-*/
-
 // 1. Crea una variable para cada operación aritmética
+
+    let suma = 1 + 3
+    let mult = 1 * 3
+    let rest = 1 - 3
+    let div = 1 / 3
 
 // 2. Crea una variable para cada tipo de operación de asignación,
 //    que haga uso de las variables utilizadas para las operaciones aritméticas
 
 // 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+    console.log(10 > 5);      // true: 10 es mayor que 5
+    console.log(8 >= 8);      // true: 8 es mayor o igual a 8
+    console.log(3 === 3);     // true: 3 es estrictamente igual a 3
+    console.log(7 !== 4);     // true: 7 es diferente de 4
+    console.log("a" < "b");   // true: 'a' tiene un valor menor que 'b' en la tabla Unicode
+
 
 // 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
 

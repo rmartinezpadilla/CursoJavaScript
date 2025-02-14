82d249331496c71efc5196a379ba7fad2ab27f11
ruben <ruben.martinez@t-evolvers.com>
2025-02-14 16:08:15 -0500

    Realizando ejercicos de variables y constantes

--- Curso/03-Ejercicios.js
+++ Curso/03-Ejercicios.js
@@ -1,19 +1,64 @@
 // 1. Escribe un comentario en una línea
+    // Comentario de una linea
 
 // 2. Escribe un comentario en varias líneas
+    /*
+        Comentarios de 
+        varias 
+        lineas
+    */
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+    let miNumero = 10
+    let miNombre = "Test QA"
+    let vive = true
+    let altura = 1.67    
 
 // 4. Imprime por consola el valor de todas las variables
+    console.log(miNumero)
+    console.log(miNombre)
+    console.log(vive)
+    console.log(altura)
 
 // 5. Imprime por consola el tipo de todas las variables
+    console.log(typeof miNumero)
+    console.log(typeof miNombre)
+    console.log(typeof vive)
+    console.log(typeof altura)
 
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+    miNombre = "Cambiando nombre"
+    miNumero = 65
+    vive = false
+    altura = 1.80
+
+    /*
+        Imprimiendo los datos
+    */
+    console.log(typeof miNumero)
+    console.log(typeof miNombre)
+    console.log(typeof vive)
+    console.log(typeof altura)
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+    miNombre = 68
+    miNumero = "texto"
+    vive = "juan"
+    altura = "texto II"
 
+     /*
+        Imprimiendo los datos
+    */
+    console.log(typeof miNumero)
+    console.log(typeof miNombre)
+    console.log(typeof vive)
+    console.log(typeof altura)
+    
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+    const pi = 3.1416
 
 // 9. A continuación, modifica los valores de las constantes
+    // pi = 30.23
 
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
+    // comentadas
